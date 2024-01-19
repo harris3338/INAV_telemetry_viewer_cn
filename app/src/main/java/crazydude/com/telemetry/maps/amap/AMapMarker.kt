02647d114e887263a096acf31c0c79412b506c89
harris3338 <harris3338@icloud.com>
2024-01-19 23:19:33 +0800

    [feat] updated Amap marker

--- app/src/main/java/crazydude/com/telemetry/maps/amap/AMapMarker.kt
+++ app/src/main/java/crazydude/com/telemetry/maps/amap/AMapMarker.kt
@@ -1,7 +1,9 @@
 package crazydude.com.telemetry.maps.amap
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Canvas
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.DrawableCompat
 import com.amap.api.maps.AMap
@@ -16,11 +18,14 @@ class AMapMarker(icon: Int, color: Int, position: Position, private val aMap: AM
     private val marker: Marker
 
     init {
-        // Convert drawable resource to BitmapDescriptor, applying the tint
         val drawable = ContextCompat.getDrawable(context, icon)
         DrawableCompat.setTint(drawable!!, color)
-        val bitmap = BitmapFactory.decodeResource(context.resources, icon)
+        val bitmap = Bitmap.createBitmap(drawable.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        drawable.setBounds(0, 0, canvas.width, canvas.height)
+        drawable.draw(canvas)
         val tintedIcon = BitmapDescriptorFactory.fromBitmap(bitmap)
+
 
         // Setup the marker options and create the marker on the map
         val markerOptions = MarkerOptions()
